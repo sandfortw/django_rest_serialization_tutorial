692e1c08a0b92bee5fafa087721c7b614df2e774
Weston Sandfort <sandfortw@gmail.com>
2023-09-15 13:23:58 -0600

    Fix serializer for snippets

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
-class SnippetSerializer(serializers.Serialier):
+class SnippetSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
     code = serializers.CharField(style={'base_template': 'textarea.html'})
@@ -9,7 +9,7 @@ class SnippetSerializer(serializers.Serialier):
     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
 
-    def create(self, validate_data):
+    def create(self, validated_data):
         return Snippet.objects.create(**validated_data)
     
     def update(self, instance, validated_data):
